0d7dafe4dce10cf5ebe137792fd3a4ec458203b7
Axel Galvan <71230877+AVGalvan@users.noreply.github.com>
2023-02-22 20:08:36 -0800

    style about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 export default function About () {
   return(
-    <div className="about">
-      <h2>About Axel</h2>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-    </div>
+      <div className="about">
+        <div className="about-intro">
+          <h2 className="larger">About Me</h2>
+          <img id="artist-picture" alt="the artist holding a boogie board with dunes in the background" src="images/website-images/axel-dunes.jpg"/>
+        </div>
+
+        <p className="width-80">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
   )
 }
